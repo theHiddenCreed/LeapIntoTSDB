2bb45429f5731cee6b3938cb44b5a01c878aa5df
tiagolofi <jejeco75@gmail.com>
2025-03-15 23:44:41 -0300

    feat: adicionar comentários para consultas MongoDB complexas

--- src/main/java/br/com/thc/service/MongoService.java
+++ src/main/java/br/com/thc/service/MongoService.java
@@ -35,5 +35,184 @@ public class MongoService {
 
         return result;
     }
+    
+    // Arrays.asList(new Document("$match", 
+    // new Document("metadata.valueType", "close")
+    //         .append("metadata.symbol", "BBSA3")
+    //         .append("timestamp", 
+    // new Document("$gte", 
+    // new java.util.Date(1741737600000L))
+    //             .append("$lt", 
+    // new java.util.Date(1741910400000L)))), 
+    // new Document("$group", 
+    // new Document("_id", 
+    // new Document("year", 
+    // new Document("$year", "$timestamp"))
+    //             .append("month", 
+    // new Document("$month", "$timestamp"))
+    //             .append("day", 
+    // new Document("$dayOfMonth", "$timestamp"))
+    //             .append("hour", 
+    // new Document("$hour", "$timestamp")))
+    //         .append("values", 
+    // new Document("$push", "$value"))
+    //         .append("maxValue", 
+    // new Document("$max", "$value"))), 
+    // new Document("$project", 
+    // new Document("_id", 0L)
+    //         .append("timestamp", 
+    // new Document("$dateFromParts", 
+    // new Document("year", "$_id.year")
+    //                 .append("month", "$_id.month")
+    //                 .append("day", "$_id.day")
+    //                 .append("hour", "$_id.hour")))
+    //         .append("medianValue", 
+    // new Document("$let", 
+    // new Document("vars", 
+    // new Document("sortedValues", 
+    // new Document("$sortArray", 
+    // new Document("input", "$values")
+    //                             .append("sortBy", 1L))))
+    //                 .append("in", 
+    // new Document("$arrayElemAt", Arrays.asList("$$sortedValues", 
+    //                         new Document("$floor", 
+    //                         new Document("$divide", Arrays.asList(new Document("$size", "$$sortedValues"), 2L))))))))
+    //         .append("maxValue", "$maxValue")))
+
+
+// Arrays.asList(new Document("$match", 
+//     new Document("metadata.valueType", "close")
+//             .append("metadata.symbol", "BBSA3")
+//             .append("timestamp", 
+//     new Document("$gte", 
+//     new java.util.Date(1741737600000L))
+//                 .append("$lt", 
+//     new java.util.Date(1741910400000L)))), 
+//     new Document("$group", 
+//     new Document("_id", 
+//     new Document("year", 
+//     new Document("$year", "$timestamp"))
+//                 .append("month", 
+//     new Document("$month", "$timestamp"))
+//                 .append("day", 
+//     new Document("$dayOfMonth", "$timestamp"))
+//                 .append("hour", 
+//     new Document("$hour", "$timestamp")))
+//             .append("values", 
+//     new Document("$push", "$value"))
+//             .append("avgValue", 
+//     new Document("$avg", "$value"))
+//             .append("sumValue", 
+//     new Document("$sum", "$value"))
+//             .append("minValue", 
+//     new Document("$min", "$value"))
+//             .append("maxValue", 
+//     new Document("$max", "$value"))), 
+//     new Document("$project", 
+//     new Document("_id", 0L)
+//             .append("timestamp", 
+//     new Document("$dateFromParts", 
+//     new Document("year", "$_id.year")
+//                     .append("month", "$_id.month")
+//                     .append("day", "$_id.day")
+//                     .append("hour", "$_id.hour")))
+//             .append("avgValue", 1L)
+//             .append("medianValue", 
+//     new Document("$arrayElemAt", Arrays.asList("$values", 
+//                     new Document("$floor", 
+//                     new Document("$divide", Arrays.asList(new Document("$size", "$values"), 2L))))))
+//             .append("sumValue", 1L)
+//             .append("minValue", 1L)
+//             .append("maxValue", 1L)))
+
+// Arrays.asList(new Document("$match", 
+//     new Document("metadata.valueType", "close")
+//             .append("timestamp", 
+//     new Document("$gte", 
+//     new java.util.Date(1741737600000L))
+//                 .append("$lt", 
+//     new java.util.Date(1741910400000L)))), 
+//     new Document("$set", 
+//     new Document("hour", 
+//     new Document("$dateTrunc", 
+//     new Document("date", "$timestamp")
+//                     .append("unit", "hour")))), 
+//     new Document("$group", 
+//     new Document("_id", 
+//     new Document("symbol", "$metadata.symbol")
+//                 .append("hour", "$hour"))
+//             .append("mean", 
+//     new Document("$avg", "$value"))
+//             .append("median", 
+//     new Document("$avg", "$value"))
+//             .append("sum", 
+//     new Document("$sum", "$value"))
+//             .append("min", 
+//     new Document("$min", "$value"))
+//             .append("max", 
+//     new Document("$max", "$value"))), 
+//     new Document("$set", 
+//     new Document("timestamp", "$_id.hour")
+//             .append("symbol", "$_id.symbol")), 
+//     new Document("$unset", "_id"))
+
+// Arrays.asList(new Document("$match", 
+//     new Document("metadata.valueType", "close")
+//             .append("timestamp", 
+//     new Document("$gte", 
+//     new java.util.Date(1741737600000L))
+//                 .append("$lt", 
+//     new java.util.Date(1741910400000L)))), 
+//     new Document("$sort", 
+//     new Document("timestamp", 1L)), 
+//     new Document("$group", 
+//     new Document("_id", 
+//     new Document("symbol", "$metadata.symbol")
+//                 .append("hour", 
+//     new Document("$hour", "$timestamp"))
+//                 .append("day", 
+//     new Document("$dayOfMonth", "$timestamp")))
+//             .append("averageValue", 
+//     new Document("$avg", "$value"))
+//             .append("values", 
+//     new Document("$push", 
+//     new Document("timestamp", "$timestamp")
+//                     .append("value", "$value")))), 
+//     new Document("$group", 
+//     new Document("_id", "$_id.symbol")
+//             .append("hourlyAverages", 
+//     new Document("$push", 
+//     new Document("hour", "$_id.hour")
+//                     .append("day", "$_id.day")
+//                     .append("avgValue", "$averageValue")
+//                     .append("values", "$values")))), 
+//     new Document("$project", 
+//     new Document("symbol", "$_id")
+//             .append("differences", 
+//     new Document("$map", 
+//     new Document("input", 
+//     new Document("$range", Arrays.asList(1L, 
+//                             new Document("$size", "$hourlyAverages"))))
+//                     .append("as", "idx")
+//                     .append("in", 
+//     new Document("$let", 
+//     new Document("vars", 
+//     new Document("current", 
+//     new Document("$arrayElemAt", Arrays.asList("$hourlyAverages", "$$idx")))
+//                                 .append("previous", 
+//     new Document("$arrayElemAt", Arrays.asList("$hourlyAverages", 
+//                                         new Document("$subtract", Arrays.asList("$$idx", 1L))))))
+//                             .append("in", 
+//     new Document("timestamp", 
+//     new Document("$arrayElemAt", Arrays.asList("$$current.values.timestamp", 0L)))
+//                                 .append("symbol", "$symbol")
+//                                 .append("diffAvg", 
+//     new Document("$subtract", Arrays.asList("$$current.avgValue", "$$previous.avgValue"))))))))), 
+//     new Document("$unwind", "$differences"), 
+//     new Document("$project", 
+//     new Document("_id", 0L)
+//             .append("timestamp", "$differences.timestamp")
+//             .append("symbol", "$differences.symbol")
+//             .append("diffAvg", "$differences.diffAvg")))
 
 }
